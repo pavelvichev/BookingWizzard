e106139031892c496d484a141284dbebe3c0b576
pavelvichev <pavelvichev15@gmail.com>
2023-05-18 19:21:48 +0300

    Changed by Local Git

---
+++ BookingWizard/BookingWizard/Migrations/20230518155012_update2Db.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWizard.Migrations
+{
+    /// <inheritdoc />
+    public partial class update2Db : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imageUrl",
+                table: "hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imageUrl",
+                table: "hotels");
+        }
+    }
+}
---
+++ BookingWizard/BookingWizard/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingWizard.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public Hotel hotel { get; set; }
+
+        public DateTime arrival_date { get; set; }
+        public DateTime date_of_departure { get; set; }
+     }
+}
