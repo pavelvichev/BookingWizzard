d8a8b3caa8b25f0a60ea2fed35ab13c99706ade7
pavelvichev <pavelvichev15@gmail.com>
2023-07-07 18:27:44 +0300

    Changed by Local Git

---
+++ BookingWizard/BookingWizard/wwwroot/Scripts/Delete.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("click", function (e) {
+	let addButton = document.getElementById('toggleButton');
+	let deleteButton = document.getElementById('deleteButton');
+	let content = document.getElementById('myContent');
+
+	if (e.target === addButton) {
+		content.style.display = content.style.display === 'none' ? 'block' : 'none';
+	}
+	else if (e.target === deleteButton) {
+		let confirmation = confirm("Are you sure you want to delete the item?");
+		if (!confirmation) {
+			e.preventDefault();
+		}
+	}
+});
